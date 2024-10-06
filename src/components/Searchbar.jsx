44afd9664967d09c26c0a124e719b5114f8aa9f8
Ealfred1 <alfrederic371@gmail.com>
2024-10-06 23:46:05 +0100

    second bar section 60% done

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -4,10 +4,12 @@ const Searchbar = () => {
 	return (
 		<nav className="bg-primary py-2 sticky top-0 z-[30]">
 			<div className="max-w-7xl flex items-center justify-between py-2 mx-auto px-4">
-			<button className="bg-black text-secondary rounded-lg h-10 w-[15%] py-1"><i className="pi pi-bars"></i> All categories</button>
+			<button className="bg-black text-secondary rounded-lg h-12 w-[15%] py-1"><i className="pi pi-bars"></i> All categories</button>
 
-			<div className="w-[50%] border border-grayDark">
-				
+			<div className="w-[50%] border border-grayDark h-12 rounded-lg flex">
+				<button className="w-[25%]">All Category <i className="pi pi-chevron-down"></i></button>
+				<input type="text" id="" className="w-[55%]" placeholder="Search products..." />
+				<button className="bg-black text-secondary rounded-lg h-12 w-[20%]">Search</button>
 			</div>
 
 			<div className="text-white text-2xl flex gap-5">
