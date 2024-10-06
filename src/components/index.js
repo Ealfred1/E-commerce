e33ffb7984c44e4bb398eae76479e3a11fbf60ac
Ealfred1 <alfrederic371@gmail.com>
2024-10-06 14:18:45 +0100

    layout and component setup complete

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,10 @@
 import Layout from './Layout'
-import HomePage from './Homepage'
 import NotFound from './NotFound'
 import Navbar from './Navbar'
 
 
 export {
 	Layout,
-	HomePage,
 	NotFound,
 	Navbar
 }
