9293ecd8119588df2392af5cbb34e73836451711
Ealfred1 <alfrederic371@gmail.com>
2024-10-06 14:15:10 +0100

    layout and component setup

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link, Outlet } from 'react-router-dom'
+import { Navbar } from './'
 
 const Layout = () => {
 	return (
 		<div>
-			
+			<Outlet />
 		</div>
 	)
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,12 @@
 import Layout from './Layout'
 import HomePage from './Homepage'
 import NotFound from './NotFound'
+import Navbar from './Navbar'
 
 
 export {
 	Layout,
 	HomePage,
-	NotFound
+	NotFound,
+	Navbar
 }
