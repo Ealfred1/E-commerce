d81b9d13dbcba225b60d64689dbd6b53a14388f1
Ealfred1 <alfrederic371@gmail.com>
2024-10-06 23:37:07 +0100

    second bar section 50% done

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -10,10 +10,22 @@ const Searchbar = () => {
 				
 			</div>
 
-			<div className="text-white text-2xl">
-				<button className=""><i className="pi pi-heart"></i></button>
-				<button className=""><i className="pi pi-cart-plus"></i></button>
-				<button className=""><i className="pi pi-user"></i></button>
+			<div className="text-white text-2xl flex gap-5">
+				<button className="hover:scale-[1.2] transition-all duration-200 flex items-center flex-col"><p><i className="pi pi-heart"></i></p>
+					<p className="text-[11px] -mt-3">
+						Wish List
+					</p>
+				</button>
+				<button className="hover:scale-[1.2] transition-all duration-200 flex items-center flex-col"><p><i className="pi pi-cart-plus"></i></p>
+					<p className="text-[11px] -mt-3">
+						 Cart
+					</p>
+				</button>
+				<button className="hover:scale-[1.2] transition-all duration-200 flex items-center flex-col"><p><i className="pi pi-user"></i></p>
+					<p className="text-[11px] -mt-3">
+						 Account
+					</p>
+				</button>
 			</div>
 			</div>
 		</nav>
