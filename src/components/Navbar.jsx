4ddf161f6451fb1e877b8dc17d4a16b506b6d60f
Ealfred1 <alfrederic371@gmail.com>
2024-10-06 23:09:20 +0100

    navbar style changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,18 +11,23 @@ const Navbar = () => {
 	return (
 		<>
 			<MobileNav isOpen={openMenu} toggleMenu={toggleMenu} />
-			<nav className="bg-[rgba(0,0,0,0.1)] py-2 sticky top-0 z-[30] backdrop-blur-3xl">
-				<div className="max-w-5xl flex items-center justify-between py-4 mx-auto px-[1.8rem] md:px-[2rem]">
+			<nav className="bg-transparent py-2 sticky top-0 z-[30] backdrop-blur-3xl">
+				<div className="flex items-center justify-between py-4 mx-auto px-[1.8rem] md:px-[2rem]">
 					<div className="text-white text-2xl font-semibold">
-						<h1 className="">Eric ✨️</h1>
+						<h1 className="text-primary">RAF<span className="text-grayDark">CART</span></h1>
 					</div>
 
+					<ul className="flex items-center gap-2 space-x-10 hidden lg:flex -translate-x-24">
+						<li className="navlink"><a href="#home">Home <i className="pi pi-chevron-down text-grayLight translate-y-0.5"></i></a></li>
+						<li className="navlink"><a href="#services">Shop <i className="pi pi-chevron-down text-grayLight translate-y-0.5"></i></a></li>
+						<li className="navlink"><a href="#projects">Pages <i className="pi pi-chevron-down text-grayLight translate-y-0.5"></i></a></li>
+						<li className="navlink"><a href="#projects">Contact</a></li>
+					</ul>
+
 					<ul className="flex items-center gap-2 space-x-10 hidden lg:flex">
-						<li className="navlink"><a href="#home">Home</a></li>
-						<li className="navlink"><a href="#services">Services</a></li>
-						<li className="navlink"><a href="#projects">Projects</a></li>
-						<li className="navlink"><a href="#skills">Skills</a></li>
-						<li className="navlink"><a href="#contact">Contact Me</a></li>
+						<li className="navlink"><a href="#home">Login/Register</a></li>
+						<li className="navlink"><a href="#services">Language <i className="pi pi-chevron-down text-grayLight translate-y-0.5"></i></a></li>
+						<li className="navlink"><a href="#projects">Currency <i className="pi pi-chevron-down text-grayLight translate-y-0.5"></i></a></li>
 					</ul>
 					
 					<button className="menu-btn" onClick={toggleMenu}><span className={`pi ${openMenu ? 'pi-times' : 'pi-bars'} text-[1rem]`}></span></button>
