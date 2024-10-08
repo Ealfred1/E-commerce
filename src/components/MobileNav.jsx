85aab6d399a6ba025b084cc46abfbf68563b2842
Ealfred1 <alfrederic371@gmail.com>
2024-10-08 02:02:17 +0100

    mobile view responsiveness (navba)

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -4,16 +4,18 @@ const MobileNav = ({ isOpen, toggleMenu }) => {
 	return (
 		<>
 			<div className={`mobile-menu ${isOpen ? 'active' : ''}`} onClick={toggleMenu}>
-				<div className="w-[60vw] h-screen bg-[#342864] p-[2rem]">
-					<div className="text-white text-2xl font-semibold mb-16">
-						<h1 className="">Eric ✨️</h1>
+				<div className="w-[60vw] h-screen bg-secondary p-[2rem]">
+					<div className="text-white text-2xl font-semibold">
+						<h1 className="text-primary mb-16">RAF<span className="text-grayDark">CART</span></h1>
 					</div>
 					<ul className="flex flex-col gap-4 ml-2 space-y-4">
-						<li className="navlink"><a href="#home">Home</a></li>
-						<li className="navlink"><a href="#services">Services</a></li>
-						<li className="navlink"><a href="#projects">Projects</a></li>
-						<li className="navlink"><a href="#skills">Skills</a></li>
-						<li className="navlink"><a href="#contact">Contact Me</a></li>
+						<li className="navlink"><a href="#home">Home <i className="pi pi-chevron-down text-grayLight translate-y-0.5"></i></a></li>
+						<li className="navlink"><a href="#services">Shop <i className="pi pi-chevron-down text-grayLight translate-y-0.5"></i></a></li>
+						<li className="navlink"><a href="#projects">Pages <i className="pi pi-chevron-down text-grayLight translate-y-0.5"></i></a></li>
+						<li className="navlink"><a href="#projects">Contact</a></li>
+						<li className="navlink"><a href="#home">Login/Register</a></li>
+						<li className="navlink"><a href="#services">Language <i className="pi pi-chevron-down text-grayLight translate-y-0.5"></i></a></li>
+						<li className="navlink"><a href="#projects">Currency <i className="pi pi-chevron-down text-grayLight translate-y-0.5"></i></a></li>
 					</ul>
 				</div>
 			</div>
